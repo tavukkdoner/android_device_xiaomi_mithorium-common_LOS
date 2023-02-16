32361b2a2ea4bf2d7d1ee98200828f85e1a6c883
me-cafebabe <me.cafebabe@gmail.com>
2023-02-17 06:38:39 +0800

    mithorium-common: Move `TARGET_SUPPORT_HAL1` flag to the right place
    * Idk why I made such a mistake when I was adding this flag...
    
    Change-Id: I8b15ffbd2089a7a59164c76a38830ec6331a71e7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+TARGET_SUPPORT_HAL1 := false
 TARGET_TS_MAKEUP := true
 
 # Display
@@ -128,9 +129,6 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware \
     /mnt/vendor/persist:/persist
-
-# Camera
-TARGET_SUPPORT_HAL1 := false
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
