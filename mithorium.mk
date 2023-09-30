161fa36af76ecc33d1b7d86040d81fa6d4681bed
me-cafebabe <me.cafebabe@gmail.com>
2023-09-30 21:42:43 +0800

    mithorium-common: Switch to QTI Health AIDL
    Change-Id: I56e0373e41506f8fd669b94502b69e0bddfe06f8

--- mithorium.mk
+++ mithorium.mk
@@ -213,8 +213,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
