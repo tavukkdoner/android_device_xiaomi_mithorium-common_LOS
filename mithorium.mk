043f49d7885019e63fa1c7d61d513ed78fa8da77
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-28 06:29:11 +0800

    mithorium-common: Use lineage radio config wrapper
    * This lets us have radio.config@1.1 interface while the modem is
      only capable of radio.config@1.0
    * Main purpose is to fix RIL after U QPR2, as radio.config@1.0 is
      no longer supported. Switching to radio.config@1.1 directly results
      in broken mobile data switch (Workaround that in telephony framework
      is possible, this way we fix it on AOSP GSIs too)
    
    Change-Id: I1dd299d45d92366005fe84c3275edded265d2da8

--- mithorium.mk
+++ mithorium.mk
@@ -381,6 +381,12 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+PRODUCT_PACKAGES += \
+    android.hardware.radio.c_shim@1.0 \
+    android.hardware.radio.c_shim@1.1 \
+    android.hardware.radio.c_shim@1.2 \
+    android.hardware.radio.config@1.1-service.wrapper
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
