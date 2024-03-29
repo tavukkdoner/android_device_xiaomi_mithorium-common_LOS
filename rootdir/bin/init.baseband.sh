65902641fa5fcb2f300098d9cbefd989041bc1af
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-30 02:36:46 +0800

    fixup! mithorium-common: Parse modem version string from /sys/devices/soc0/images
    Change-Id: I10336dbaa8211ea3fe7f2e1bfb7b75e24cfa34c5

--- rootdir/bin/init.baseband.sh
+++ rootdir/bin/init.baseband.sh
@@ -2,6 +2,6 @@
 
 baseband_str=$(grep -Eo 'MPSS.*' /sys/devices/soc0/images)
 
-if [ ! -z $baseband_str ]; then
+if [ "$baseband_str" ]; then
     setprop vendor.gsm.version.baseband $baseband_str
 fi
