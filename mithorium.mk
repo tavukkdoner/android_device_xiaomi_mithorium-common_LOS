9383b60210d16a9f709ee07aec669d8206ddce22
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-22 22:02:13 +0800

    fixup! mithorium-common: Build missing libraries for 14 QPR3
    Change-Id: Ibfb72c22e98fb806993c0ace4a26037d4a55eb4b

--- mithorium.mk
+++ mithorium.mk
@@ -396,6 +396,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
+    libpower.vendor:64 \
     libsensorndkbridge
 
 # Shims
